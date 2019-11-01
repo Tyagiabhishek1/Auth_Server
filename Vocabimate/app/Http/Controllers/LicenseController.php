b71bf9d5a00255a882170037aa70557761f3712d
Abhishek Tyagi <abhishektyagi268@gmail.com>
2019-11-01 16:39:49 +0530

    licSer

--- Vocabimate/app/Http/Controllers/LicenseController.php
+++ Vocabimate/app/Http/Controllers/LicenseController.php
@@ -16,31 +16,31 @@ class LicenseController extends Controller
     {
         $user = Auth::user();
         $id=$user->user_id;
-        $subs=DB::table('subscription')
+        $user_subscription_plan=DB::table('subscription')
         ->select('subscription.sub_start_date','subscription.sub_end_date','subscription.subs_status','subscription.user_id')
         ->where("subscription.user_id", "=",$id)
         ->get();
-        $sub_plan=DB::table('subscription')
+        $user_plan_details=DB::table('subscription')
       ->select('subscription.plan_id','plan.plan_name','plan.plan_description','plan.time_period_months','plan.actual_price','plan.discount_price','plan.is_hd_available','plan.is_uhd_available','plan.can_download','plan.number_of_device')
       ->join('plan','plan.plan_id','=','subscription.plan_id')
       ->where("subscription.user_id", "=",$id)
       ->get();
-      $sub=DB::table('subscription')
+      $user_plan_status=DB::table('subscription')
       ->select('subscription.sub_id')
       ->join('plan','plan.plan_id','=','subscription.plan_id')
       ->where("subscription.user_id", "=",$id)
       ->get();
 
-      return response()->json(['subscription' =>$sub,'plan'=>$sub_plan,'status'=>$subs]);
+      return response()->json(['subscription' =>$user_subscription_plan,'plan'=>$user_plan_details,'status'=>$user_plan_status]);
 
     }
     public function getplanlist()
     {
         $user = Auth::user();
-        $plan=DB::table('plan')
+        $get_plan_list=DB::table('plan')
       ->select('plan.plan_id','plan.plan_name','plan.plan_description','plan.time_period_months','plan.actual_price','plan.discount_price','plan.is_hd_available','plan.is_uhd_available','plan.number_of_device','plan.can_download')
       ->get();
-      return response()->json(['plan' =>$plan]);
+      return response()->json(['plan' =>$get_plan_list]);
 
     }
     public function upgradeplan(Request $request)
@@ -55,19 +55,19 @@ if ($validator->fails()) {
 }  
     $input = $request->all(); 
     $id=$user->user_id;
-    $update['user_id']=$input['user_id'];
-    $update['plan_id']=$input['plan_id'];
-    $new=$input['plan_id'];
-    $update['sub_start_date']=$dt;
-    $sql="select plan_id,plan_name,actual_price,discount_price from plan where plan_id=$new";
-    $comments = DB::select($sql);
+    $upgrade_plan['user_id']=$input['user_id'];
+    $upgrade_plan['plan_id']=$input['plan_id'];
+    $new_plan_id=$input['plan_id'];
+    $upgrade_plan['sub_start_date']=$dt;
+    $sql="select plan_id,plan_name,actual_price,discount_price from plan where plan_id=$new_plan_id";
+    $user_new_plan = DB::select($sql);
    
-   foreach ($comments as $comments) {
+   foreach ($user_new_plan as $user_new_plan) {
                 
-      $plan_name= $comments->plan_name;
-      $actual_price= $comments->actual_price;
-      $plan_id= $comments->plan_id;
-      $discount_price= $comments->discount_price;
+      $plan_name= $user_new_plan->plan_name;
+      $actual_price= $user_new_plan->actual_price;
+      $plan_id= $user_new_plan->plan_id;
+      $discount_price= $user_new_plan->discount_price;
 
 
 
@@ -75,8 +75,8 @@ if ($validator->fails()) {
    DB::table('subscription')
    ->where('user_id',$id)
    ->update(array('plan_id'=>$plan_id,'sub_start_date'=>$dt,'actual_price'=>$actual_price,'discount_price'=>$discount_price));
-    $code = array("code"=>200,'message'=>'Your Plan Has Been Upgraded to '.$plan_name);
+    $response = array("code"=>200,'message'=>'Your Plan Has Been Upgraded to '.$plan_name);
 
-      return response()->json(['success' =>$code]); 
+      return response()->json(['success' =>$response]); 
     }
 }
