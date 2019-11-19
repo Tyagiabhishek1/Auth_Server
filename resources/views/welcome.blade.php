d40030c5016599fd23cc68c6aed5c10e55c1338f
Abhishek Tyagi <abhishektyagi268@gmail.com>
2019-11-19 18:32:20 +0530

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Vocabimate Auth Server
                 </div>
 
                 <div class="links">
